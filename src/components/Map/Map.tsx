cd960b57891cc439857dd494c33b47d7dbdcac6a
Karol M <keszua@gmail.com>
2023-02-18 00:04:24 +0100

    Dodaj mape leaflet

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,22 @@
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import './Map.css';
+import 'leaflet/dist/leaflet.css';
+import '../../utils/fix-map-icon';
 
 export const Map = () => {
     return (
         <div className="map">
-             <p>Mapa</p>
+            <MapContainer center={[51.4097, 21.1301]} zoom={18} scrollWheelZoom={false}> 
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"           
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[51.4099, 21.1301]} >
+                    <Popup>
+                        Twoja pozycja
+                    </Popup>
+                </Marker>    
+            </MapContainer>
         </div>
     );
 };
