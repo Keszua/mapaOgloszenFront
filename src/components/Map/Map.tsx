edd37b4dc77e2d30c9e5c84e4d9c41c69c8b904e
Karol_M <keszua@gmail.com>
2023-02-20 16:08:16 +0100

    Przebudowa mapy ( w trakcie)

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,15 +2,26 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
 import '../../utils/fix-map-icon';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SearchContext } from '../contexts/search.context';
+import { SimpleAdEntity } from 'types';
 
 export const Map = () => {
     const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
+
+    // useEffect( () => {
+    //     console.log('Make request');
+    // },[search]);
 
     useEffect( () => {
-        console.log('Make request');
-    },[search]);
+        (async () => {
+            const res = fetch('http://loclahost:3001/ad/search');
+            const data = await (await res).json();
+
+            setAds(data);
+        })();
+    },[]);
 
     return (
         <div className="map">
@@ -22,11 +33,15 @@ export const Map = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"           
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={[51.4099, 21.1301]} >
-                    <Popup>
-                        Twoja pozycja
-                    </Popup>
-                </Marker>    
+                {
+                    ads.map(ad => (
+                        <Marker position={[51.4099, 21.1301]} >
+                            <Popup>
+                                Twoja pozycja
+                            </Popup>
+                        </Marker>    
+                    ))
+                }
             </MapContainer>
         </div>
     );
