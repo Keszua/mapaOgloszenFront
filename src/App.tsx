2912501ef7511634b478f99b94c3076dbb257c5f
Karol_M <keszua@gmail.com>
2023-02-20 13:46:45 +0100

    Dodaj useContext dla wyszukiwarki

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/layout/Header';
 import { Map } from './components/Map/Map';
+import { SearchContext } from './components/contexts/search.context';
 
 
 export const App = () => {
+    const [search, setSearch] = useState<string>('');
 
   return <>
-    <div className="wrapper">
-      <Header />
-      <Map />
+    <div className="wrapper_">
+        <SearchContext.Provider value={{
+            search,
+            setSearch,
+        }}>
+            <Header />
+            <Map />
+        </SearchContext.Provider>
     </div>
   </>;
 };
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,10 +2,21 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
 import '../../utils/fix-map-icon';
+import { useContext, useEffect } from 'react';
+import { SearchContext } from '../contexts/search.context';
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+
+    useEffect( () => {
+        console.log('Make request');
+    },[search]);
+
     return (
         <div className="map">
+            <h1>
+                Search for: {search}
+            </h1>
             <MapContainer center={[51.4097, 21.1301]} zoom={18} scrollWheelZoom={false}> 
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"           
